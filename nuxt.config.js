e5c72a1211c336ede00e9006885f88f279b6f2b4
Ruud <ruudniew@protonmail.com>
2019-11-12 13:04:35 +0100

    nux server config

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,10 +58,11 @@ module.exports = {
     */
     extend (config, ctx) {
     }
-  },
-
-  server: {
-    "host": "127.0.0.1",
-    "port": "3101"
   }
+  // ,
+  //
+  // server: {
+  //   "host": "127.0.0.1",
+  //   "port": "3101"
+  // }
 }
