8f7d411e9be9331439dc23c3332d45c4e122a072
Ruud <ruudniew@protonmail.com>
2019-11-22 18:04:07 +0100

    analytics

--- static/mtm.js
+++ static/mtm.js
@@ -1,5 +1,4 @@
 <!-- Matomo -->
-<script type="text/javascript">
 var _paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
@@ -11,5 +10,4 @@ _paq.push(['enableLinkTracking']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
-</script>
 <!-- End Matomo Code -->
